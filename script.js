c4e9840b5fb20fa137804e8842bbc95d60ab89e1
zubsyeda <125157533+zubsyeda@users.noreply.github.com>
2023-04-26 20:03:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ $(document).ready(function() {
     $(".content-box").load("Articles/haunted.html");
     }
     else if ($(this).hasClass("manjiri")) {
-      $(".content-box").load("Articles/manjeri.html");
+      $(".content-box").load("Articles/Manjeri.html");
       }
     else {
       $(".content-box").load($(this).text() + ".html");
